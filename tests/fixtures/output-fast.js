13aa36a29bad162d0a3ab77fbd32ba58ea8624da
Stefan Penner <stefan.penner@gmail.com>
2014-05-11 22:54:22 -0400

    some more ideas re: fast super + more inheritance depth

--- tests/fixtures/output-fast.js
+++ tests/fixtures/output-fast.js
@@ -1,35 +1,65 @@
+function Bar() {
+
+}
+
+Bar.prototype = null;
+Bar.prototype.constructor = Bar;
+Bar.prototype.apple = function() {
+
+};
+
+function Foo(options) {
+  this._super$1constructor1(options);
+}
+
+Foo.prototype = Object.create(Bar.prototype);
+Foo.prototype.constructor = Foo;
+Foo.prototype._super$1constructor = Bar;
+Foo.prototype._super$1apple = Bar.prototype.apple;
+
+Foo.prototype.apple = function(camera) {
+  this._super$1apple(camera, foo);
+};
+
+Foo.prototype.orange = function() {
+
+};
+
 function Controller(options) {
   this.firstName = 'stefan';
   this.lastName = 'penner';
 
-  Foo.call(this, options); // could in theory inline this...
+  this._super$2constructor(options);
 }
 
-Controller.prototype = Object.create(Foo);
+Controller.prototype = Object.create(Foo.prototype);
 
 Controller.prototype.constructor = Controller;
 
-Controller.prototype._super$orange = Foo.prototype.orange;
+Controller.prototype._super$1constructor = Bar;
+Controller.prototype._super$2constructor = Foo;
 
-Controller.prototype._super$update = Foo.prototype.update;
+Controller.prototype._super$2orange = Foo.prototype.orange;
 
-Controller.prototype._super$apple  = Foo.prototype.apple;
+Controller.prototype._super$2update = Foo.prototype.update;
+
+Controller.prototype._super$2apple  = Foo.prototype.apple;
 
 Controller.prototype.update = function(camera) {
-  this._super$orange(camera, foo);
-  this._super$update(camera, foo);
+  this._super$2orange(camera, foo);
+  this._super$2update(camera, foo);
 };
 
 Controller.prototype.apple = function(camera) {
-  this._super$apple(camera, foo);
+  this._super$2apple(camera, foo);
 };
 
 Controller.prototype.orange = function() {
-  this._super$orange.apply(this, arguments);
+  this._super$2orange.apply(this, arguments);
 };
 
 Controller.prototype.foo = function() {
-  this._super$orange.bar(arguments);
+  this._super$2orange.bar(arguments);
 };
 
 Controller.prototype.fullName = function() {
